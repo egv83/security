c7b87dbb4930e4ca72de237a9e2108b90e955d7b
egv83 <esteban8305@hotmail.com>
2024-08-11 10:33:55 -0500

    evm - creación proyecto

---
+++ src/test/java/com/ochobits/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ochobits.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
